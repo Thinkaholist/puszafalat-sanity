4e6cad844a58a9d1317bddd960f2555951318535
Richard <35897699+nemethricsi@users.noreply.github.com>
2021-12-15 23:11:45 +0100

    add simple Explanation and courseType document (#2)

---
+++ schemas/documents/courseType.js
@@ -0,0 +1,33 @@
+export default {
+  title: 'Course Type',
+  name: 'courseType',
+  type: 'document',
+  fields: [
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'localeString',
+    },
+    {
+      title: 'URL identifier (slug)',
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'name.hu',
+        maxLength: 100,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Plural',
+      name: 'plural',
+      type: 'localeString',
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name.hu',
+    },
+  },
+};
