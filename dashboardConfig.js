c3dce4a04f6da4b011e044a4c7cea48ab7b07e2f
Richárd Németh <nemethricsi@gmail.com>
2022-02-21 23:06:22 +0100

    add both websites to Sanity

--- dashboardConfig.js
+++ dashboardConfig.js
@@ -9,22 +9,28 @@ export default {
         description:
           'A weboldalad azért tud olyan gyors lenni, mert előre elkészül a szerveren (ún. build folyamat), statikus. Ha kész vagy a változtatásokkal, a "Deploy" gombbal szükséges egy új "oldalépítést" (build) generálnod. (Elég egyszer megnyomni a gombot). A Netlify badge sárgára fog változni (Building). Pár perc az egész! Ha a Netlify gomb ismét zöld lesz (Success), az oldalépítés sikeres volt, és a változtatásokat látnod kell a weboldaladon. Ha esetleg az építés sikertelen volt, a gomb pirosra változik(Failed), ebben az esetben semmilyen változás nem történik a weboldaladon, a régi verzió ugyanúgy él tovább. Szólj, és megnézzük, hol a hiba. A "view" link a weboldalra visz, az "admin"-ra kattintva pedig a Netlify build folyamatot lehet nyomon követni.',
         sites: [
-          {
-            title: isDevelopment ? 'DEV Weboldal' : 'Éles Weboldal',
-            apiId: isDevelopment
-              ? 'acc5798e-23f0-42e3-b773-6d2a8f886fd2'
-              : 'd3fc8059-09eb-4a5f-9359-5ec2a24a8935',
-            buildHookId: isDevelopment
-              ? '61f6817ec78544f819b42f0f'
-              : '61f6845f00f5259c3b5c435d',
-            name: isDevelopment ? 'puszafalat-dev' : 'puszafalat',
-          },
           // {
-          //   title: 'DEV Weboldal',
-          //   apiId: 'acc5798e-23f0-42e3-b773-6d2a8f886fd2',
-          //   buildHookId: '61f6817ec78544f819b42f0f',
-          //   name: 'puszafalat-dev',
+          //   title: isDevelopment ? 'DEV Weboldal' : 'Éles Weboldal',
+          //   apiId: isDevelopment
+          //     ? 'acc5798e-23f0-42e3-b773-6d2a8f886fd2'
+          //     : 'd3fc8059-09eb-4a5f-9359-5ec2a24a8935',
+          //   buildHookId: isDevelopment
+          //     ? '61f6817ec78544f819b42f0f'
+          //     : '61f6845f00f5259c3b5c435d',
+          //   name: isDevelopment ? 'puszafalat-dev' : 'puszafalat',
           // },
+          {
+            title: 'Éles Weboldal',
+            apiId: 'd3fc8059-09eb-4a5f-9359-5ec2a24a8935',
+            buildHookId: '61f6845f00f5259c3b5c435d',
+            name: 'puszafalat',
+          },
+          {
+            title: 'DEV Weboldal',
+            apiId: 'acc5798e-23f0-42e3-b773-6d2a8f886fd2',
+            buildHookId: '61f6817ec78544f819b42f0f',
+            name: 'puszafalat-dev',
+          },
         ],
       },
     },
