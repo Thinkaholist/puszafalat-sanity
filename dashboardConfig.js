5fb5d6e711b262e487a2cf44c717e50c3cf97121
Richárd Németh <nemethricsi@gmail.com>
2022-02-21 23:01:21 +0100

    upgrade

--- dashboardConfig.js
+++ dashboardConfig.js
@@ -1,4 +1,4 @@
-// const isDevelopment = process.env.NODE_ENV === 'development';
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 export default {
   widgets: [
@@ -9,22 +9,22 @@ export default {
         description:
           'A weboldalad azért tud olyan gyors lenni, mert előre elkészül a szerveren (ún. build folyamat), statikus. Ha kész vagy a változtatásokkal, a "Deploy" gombbal szükséges egy új "oldalépítést" (build) generálnod. (Elég egyszer megnyomni a gombot). A Netlify badge sárgára fog változni (Building). Pár perc az egész! Ha a Netlify gomb ismét zöld lesz (Success), az oldalépítés sikeres volt, és a változtatásokat látnod kell a weboldaladon. Ha esetleg az építés sikertelen volt, a gomb pirosra változik(Failed), ebben az esetben semmilyen változás nem történik a weboldaladon, a régi verzió ugyanúgy él tovább. Szólj, és megnézzük, hol a hiba. A "view" link a weboldalra visz, az "admin"-ra kattintva pedig a Netlify build folyamatot lehet nyomon követni.',
         sites: [
-          // {
-          //   title: isDevelopment ? 'DEV Weboldal' : 'Weboldal',
-          //   apiId: isDevelopment
-          //     ? 'acc5798e-23f0-42e3-b773-6d2a8f886fd2'
-          //     : 'd3fc8059-09eb-4a5f-9359-5ec2a24a8935',
-          //   buildHookId: isDevelopment
-          //     ? '61f6817ec78544f819b42f0f'
-          //     : '61f6845f00f5259c3b5c435d',
-          //   name: isDevelopment ? 'puszafalat-dev' : 'puszafalat',
-          // },
           {
-            title: 'DEV Weboldal',
-            apiId: 'acc5798e-23f0-42e3-b773-6d2a8f886fd2',
-            buildHookId: '61f6817ec78544f819b42f0f',
-            name: 'puszafalat-dev',
+            title: isDevelopment ? 'DEV Weboldal' : 'Éles Weboldal',
+            apiId: isDevelopment
+              ? 'acc5798e-23f0-42e3-b773-6d2a8f886fd2'
+              : 'd3fc8059-09eb-4a5f-9359-5ec2a24a8935',
+            buildHookId: isDevelopment
+              ? '61f6817ec78544f819b42f0f'
+              : '61f6845f00f5259c3b5c435d',
+            name: isDevelopment ? 'puszafalat-dev' : 'puszafalat',
           },
+          // {
+          //   title: 'DEV Weboldal',
+          //   apiId: 'acc5798e-23f0-42e3-b773-6d2a8f886fd2',
+          //   buildHookId: '61f6817ec78544f819b42f0f',
+          //   name: 'puszafalat-dev',
+          // },
         ],
       },
     },
@@ -42,11 +42,11 @@ export default {
             value: 'https://github.com/Thinkaholist/puszafalat-frontend',
             category: 'Code',
           },
-          //   {
-          //     title: 'Frontend Deploy',
-          //     value: 'https://puszafalat.eu',
-          //     category: 'apps',
-          //   },
+          {
+            title: 'Frontend Deploy',
+            value: 'https://puszafalat.eu',
+            category: 'apps',
+          },
         ],
       },
       layout: {
